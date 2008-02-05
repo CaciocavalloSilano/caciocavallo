43a9a10006d3021dfa8304bc2cbb32a87316b6db
Mario Torre <152881+neugens@users.noreply.github.com>
2008-02-05 16:01:41 +0100

    just an example change

---
+++ src/Test.java
@@ -0,0 +1,21 @@
+/**
+ * 
+ */
+
+/**
+ * @author neugens
+ *
+ */
+public class Test
+{
+
+  /**
+   * @param args
+   */
+  public static void main(String[] args)
+  {
+    // TODO Auto-generated method stub
+
+  }
+
+}
