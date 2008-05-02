5bd25bdf6bb6f2b6a36893bbb0a2ede8e61e6884
Roman Kennke <roman.kennke@aicas.com>
2008-05-02 12:43:16 +0200

    Implement blitting of buffered images.

--- src/gnu/java/awt/peer/x/EscherBlitLoops.java
+++ src/gnu/java/awt/peer/x/EscherBlitLoops.java
@@ -2,10 +2,13 @@ package gnu.java.awt.peer.x;
 
 import gnu.x11.Drawable;
 import gnu.x11.GC;
+import gnu.x11.image.ZPixmap;
 
 import java.awt.Composite;
+import java.awt.image.BufferedImage;
 
 import sun.awt.SunToolkit;
+import sun.awt.image.BufImgSurfaceData;
 import sun.java2d.SurfaceData;
 import sun.java2d.loops.Blit;
 import sun.java2d.loops.CompositeType;
@@ -25,7 +28,8 @@ class EscherBlitLoops
 //        new EscherBlitLoops(SurfaceType.IntArgb,
 //                            XDrawableSurfaceData.EscherIntRgb, false),
         new EscherBlitLoops(XDrawableSurfaceData.EscherIntRgb,
-                            XDrawableSurfaceData.EscherIntRgb, true)
+                            XDrawableSurfaceData.EscherIntRgb, true),
+        new EscherBlitLoops(SurfaceType.Any, SurfaceType.Any, true)
       };
       GraphicsPrimitiveMgr.register(primitives);
   }
@@ -45,13 +49,39 @@ class EscherBlitLoops
                    int w, int h)
   {
     SunToolkit.awtLock();
-    XDrawableSurfaceData sxdsd = (XDrawableSurfaceData) src;
-    XDrawableSurfaceData dxdsd = (XDrawableSurfaceData) dst;
-    GC gc = dxdsd.getBlitGC(clip);
-    Drawable d = dxdsd.getDrawable();
-    Drawable s = sxdsd.getDrawable();
-    d.copy_area(s, gc, sx, sy, w, h, dx, dy);
-    d.display.flush();
+    if (src instanceof BufImgSurfaceData)
+      blitBufImg((BufImgSurfaceData) src, dst, comp, clip, sx, sy, dx, dy, w, h);
+    else
+      {
+        XDrawableSurfaceData sxdsd = (XDrawableSurfaceData) src;
+        XDrawableSurfaceData dxdsd = (XDrawableSurfaceData) dst;
+        GC gc = dxdsd.getBlitGC(clip);
+        Drawable d = dxdsd.getDrawable();
+        Drawable s = sxdsd.getDrawable();
+        d.copy_area(s, gc, sx, sy, w, h, dx, dy);
+        d.display.flush();
+      }
     SunToolkit.awtUnlock();
+  }
+
+  private void  blitBufImg(BufImgSurfaceData src, SurfaceData dst,
+                           Composite comp, Region clip, int sx, int sy, int dx,
+                           int dy, int w, int h)
+  {
+    // TODO: Implement transparency.
+    BufferedImage bufImg = (BufferedImage) src.getDestination();
+    XDrawableSurfaceData dxdsd = (XDrawableSurfaceData) dst;
+    Drawable d = dxdsd.getDrawable();
+    GC gc = dxdsd.getBlitGC(clip);
+    ZPixmap pm = new ZPixmap(gc.display, w, h);
+    for (int y = sy; y < sy + h; y++)
+      {
+        for (int x = sx; x < sx + w; x++)
+          {
+            int rgb = bufImg.getRGB(x, y);
+            pm.set(x, y, rgb);
+          }
+      }
+    d.put_image(gc, pm, dx, dy);
   }
 }
