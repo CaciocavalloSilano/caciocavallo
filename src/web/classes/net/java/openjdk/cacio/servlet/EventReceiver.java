a603b9f56efe6683e597bc895627ac10dfdf702b
ceisserer <ceisserer@localhost>
2011-06-29 15:28:59 +0200

    first AppContext experiments

--- src/web/classes/net/java/openjdk/cacio/servlet/EventReceiver.java
+++ src/web/classes/net/java/openjdk/cacio/servlet/EventReceiver.java
@@ -21,15 +21,6 @@ public class EventReceiver extends HttpServlet {
      */
     public EventReceiver() {
 	super();
-	// TODO Auto-generated constructor stub
-    }
-
-    /**
-     * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
-     *      response)
-     */
-    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-	// TODO Auto-generated method stub
     }
 
     /**
@@ -37,16 +28,9 @@ public class EventReceiver extends HttpServlet {
      *      response)
      */
     protected  void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-	
-//	try {
-//	    Thread.sleep(100);
-//	} catch (InterruptedException e) {
-//	    e.printStackTrace();
-//	}
-//	
+
 	String parameters = request.getParameter("events");
 	HttpSession session = request.getSession(false);
-	//System.out.println("Image-Stream Session: " + session.getId());
 	String subSessionID = request.getParameter("subsessionid");
 
 	if (session == null || subSessionID == null) {
@@ -54,21 +38,19 @@ public class EventReceiver extends HttpServlet {
 	}
 	WebSessionState.register(session, Integer.parseInt(subSessionID));
 
-	WebSessionState currentState = WebSessionState.getCurrentState();
+	WebSessionState currentState = WebSessionState.getCurrentState(session, Integer.parseInt(subSessionID));
 	currentState.lockSession();
 
 	WebScreen screen = currentState.getGraphicsConfiguration().getScreen();
 
-	parseEventData(parameters, screen);
+	parseEventData(parameters, currentState, screen);
 
 	currentState.unlockSession();
 	WebSessionState.unregister();
     }
 
-    protected void parseEventData(String paramStr, WebScreen screen) {
+    protected void parseEventData(String paramStr, WebSessionState state, WebScreen screen) {
 	String[] params = paramStr.split("_");
-
-//	System.out.println("Event-Anfrage: " + paramStr);
 
 	LinkedList<String> eventDataList = new LinkedList<String>();
 	eventDataList.addAll(Arrays.asList(params));
@@ -78,42 +60,41 @@ public class EventReceiver extends HttpServlet {
 	    if (command.length() > 0) {
 
 		if (command.equals("M")) {
-		    processMouseEvent(eventDataList);
+		    processMouseEvent(state, eventDataList);
 		} else if (command.equals("MM")) {
-		    processMouseMotionEvent(eventDataList);
+		    processMouseMotionEvent(state, eventDataList);
 		} else 
 		if(command.equals("K")) {
-		    processKeyEvent(eventDataList);
+		    processKeyEvent(state, eventDataList);
 		}
 	    }
 	}
     }
 
-    protected void processKeyEvent(LinkedList<String> params) {
+    protected void processKeyEvent(WebSessionState state, LinkedList<String> params) {
 	boolean down = Integer.parseInt(params.removeFirst()) > 0;
 	int keySym = Integer.parseInt(params.removeFirst());
-//	char charVal = (char) Integer.parseInt(params.removeFirst());
 	char charVal = 0;
 	boolean ctrl = Boolean.parseBoolean(params.removeFirst());
 	boolean shift = Boolean.parseBoolean(params.removeFirst());
 	boolean alt = Boolean.parseBoolean(params.removeFirst());
-	WebSessionState.getCurrentState().getKeyboardTracker().trackKeyEvent(down, keySym, charVal, ctrl, shift, alt);
+	state.getKeyboardTracker().trackKeyEvent(down, keySym, charVal, ctrl, shift, alt);
     }
 
-    protected void processMouseMotionEvent(LinkedList<String> params) {
+    protected void processMouseMotionEvent(WebSessionState state, LinkedList<String> params) {
 	int x = Integer.parseInt(params.removeFirst());
 	int y = Integer.parseInt(params.removeFirst());
 
-	WebSessionState.getCurrentState().getMouseTracker().trackMouseMotionEvent(x, y);
+	state.getMouseTracker().trackMouseMotionEvent(x, y);
     }
 
-    protected void processMouseEvent(LinkedList<String> params) {
+    protected void processMouseEvent(WebSessionState state, LinkedList<String> params) {
 	int x = Integer.parseInt(params.removeFirst());
 	int y = Integer.parseInt(params.removeFirst());
 	boolean down = Integer.parseInt(params.removeFirst()) > 0;
 	int buttonMask = Integer.parseInt(params.removeFirst());
 	
-	WebSessionState.getCurrentState().getMouseTracker().trackMouseEvent(down, buttonMask, x, y);
+	state.getMouseTracker().trackMouseEvent(down, buttonMask, x, y);
     }
 
 }
--- src/web/classes/net/java/openjdk/cacio/servlet/ImageStreamer.java
+++ src/web/classes/net/java/openjdk/cacio/servlet/ImageStreamer.java
@@ -1,4 +1,5 @@
 package net.java.openjdk.cacio.servlet;
+
 import java.awt.*;
 
 import java.awt.image.*;
@@ -7,6 +8,8 @@ import java.util.*;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.*;
+
+import sun.awt.*;
 import net.java.openjdk.awt.peer.web.*;
 
 /**
@@ -21,7 +24,6 @@ public class ImageStreamer extends HttpServlet {
      */
     public ImageStreamer() {
 	super();
-	// TODO Auto-generated constructor stub
     }
 
     /**
@@ -29,77 +31,66 @@ public class ImageStreamer extends HttpServlet {
      *      response)
      */
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-	
-//	try {
-//	    Thread.sleep(100);
-//	} catch (InterruptedException e) {
-//	    e.printStackTrace();
-//	}
-	
+
 	HttpSession session = request.getSession(false);
-	//System.out.println("Image-Stream Session: "+session.getId());
 	String subSessionID = request.getParameter("subsessionid");
-	
-	if(session == null || subSessionID == null) {
+
+	if (session == null || subSessionID == null) {
 	    throw new RuntimeException("Should not reach");
 	}
 
 	WebSessionState.register(session, Integer.parseInt(subSessionID));
 	try {
-	response.setContentType("text/plain");
+	    response.setContentType("text/plain");
 
-	ScreenUpdate update = getDirtyRectangle();
-	
-	if(update != null) {
-	    OutputStream str = response.getOutputStream();
-	    str.write((update.getX() + ":" + update.getY() + ":").getBytes("UTF-8"));
-	    str.write(update.getImageData());
-	}
-	
-	}finally {
+	    ScreenUpdate update = getDirtyRectangle(session, Integer.parseInt(subSessionID));
+
+	    if (update != null) {
+		OutputStream str = response.getOutputStream();
+		str.write((update.getX() + ":" + update.getY() + ":").getBytes("UTF-8"));
+		str.write(update.getImageData());
+	    }
+
+	} finally {
 	    WebSessionState.unregister();
 	}
     }
 
     int imgCnt = 0;
-    protected ScreenUpdate getDirtyRectangle() {
 
-	boolean dataFound = false;
+    protected ScreenUpdate getDirtyRectangle(HttpSession session, int subSessionID) {
 	int cnt = 0;
 	while (cnt < 1000) {
-	    long start = System.currentTimeMillis();
-
 	    Rectangle unionRect = null;
 	    BufferedImage bImg = null;
 	    int x1 = 0, y1 = 0, x2 = 0, y2 = 0;
-	    WebSessionState state = WebSessionState.getCurrentState();
-	    
-	    state.lockSession();
-	    
-	    WebGraphicsConfiguration config = state.getGraphicsConfiguration();
-	//    System.out.println("Config: "+config);
-	    if(config != null) {
-	    WebScreen screen = config.getScreen();
-	    WebSurfaceData screenSurface = screen.getSurfaceData();
-	    synchronized (screenSurface.dirtyRects) {
-		//SunToolkit.awtLock();
 
-		ArrayList<Rectangle> dirtyRectList = screenSurface.dirtyRects;
-		// System.out.println("Dirty-Rect-Size: "+dirtyRectList.size());
-		
-		// Zusammenhaengenden bereich finden
-		if (dirtyRectList.size() > 0) {
-		    unionRect = dirtyRectList.get(0);
-		    for (Rectangle rect : dirtyRectList) {
-			unionRect = unionRect.union(rect);
+	    WebSessionState state = WebSessionState.getCurrentState(session, subSessionID);
+	    SunToolkit.awtLock();
+
+	    state.lockSession();
+
+	    WebGraphicsConfiguration config = state.getGraphicsConfiguration();
+	    // System.out.println("Config: "+config);
+	    if (config != null) {
+		WebScreen screen = config.getScreen();
+		WebSurfaceData screenSurface = screen.getSurfaceData();
+		synchronized (screenSurface.dirtyRects) {
+
+		    ArrayList<Rectangle> dirtyRectList = screenSurface.dirtyRects;
+		    // System.out.println("Dirty-Rect-Size: "+dirtyRectList.size());
+
+		    // Zusammenhaengenden bereich finden
+		    if (dirtyRectList.size() > 0) {
+			unionRect = dirtyRectList.get(0);
+			for (Rectangle rect : dirtyRectList) {
+			    unionRect = unionRect.union(rect);
+			}
+			dirtyRectList.clear();
 		    }
-		    dirtyRectList.clear();
 		}
-	    }
 
 		if (unionRect != null && unionRect.width > 0 && unionRect.height > 0) {
-		    // BufferedImage bImg = new BufferedImage(unionRect.width,
-		    // unionRect.height, BufferedImage.TYPE_INT_RGB);
 		    bImg = new BufferedImage(unionRect.width, unionRect.height, BufferedImage.TYPE_INT_RGB);
 		    Graphics g = bImg.getGraphics();
 
@@ -125,48 +116,41 @@ public class ImageStreamer extends HttpServlet {
 		    g.drawImage(screenSurface.imgBuffer, 0, 0, unionRect.width, unionRect.height, x1, y1, x2, y2, null);
 		}
 
-	    
-	    state.unlockSession();
+		state.unlockSession();
+		SunToolkit.awtUnlock();
 
-	    // SunToolkit.awtUnlock();
+		// g.drawImage(WebSurfaceData.imgBuffer, 0, 0, null);
 
-	    // g.drawImage(WebSurfaceData.imgBuffer, 0, 0, null);
+		if (bImg != null) {
+		    byte[] bData = null;
+		    ByteArrayOutputStream bos = new ByteArrayOutputStream(8192);
+		    try {
+			// ImageIO.write(bImg, "png", bos);
+			bData = new PngEncoderB(bImg, false, PngEncoder.FILTER_NONE, 2).pngEncode();
 
-	    if (bImg != null) {
-		byte[] bData = null;
-		ByteArrayOutputStream bos = new ByteArrayOutputStream(8192);
-		try {
-//		    ImageIO.write(bImg, "png", bos);
-		    bData = new PngEncoderB(bImg, false, PngEncoder.FILTER_NONE, 2).pngEncode();
-		    
-		} catch (Exception e) {
-		    e.printStackTrace();
+		    } catch (Exception e) {
+			e.printStackTrace();
+		    }
+
+		    // bData = bos.toByteArray();
+
+//		    try {
+//			FileOutputStream fos = new FileOutputStream("/home/ce/imgFiles/" + imgCnt + ".png");
+//			fos.write(bData);
+//			fos.close();
+//			imgCnt++;
+//		    } catch (Exception ex) {
+//			ex.printStackTrace();
+//		    }
+
+		    byte[] data = Base64Coder.encode(bData);
+		    return new ScreenUpdate(unionRect.x, unionRect.y, data);
 		}
-
-//		bData = bos.toByteArray();
-		
-//		try {
-//		    FileOutputStream fos = new FileOutputStream("/home/ce/imgFiles/"+imgCnt+".png");
-//		    fos.write(bData);
-//		    fos.close();
-//		    imgCnt++;
-//		}catch(Exception ex) {
-//		    ex.printStackTrace();
-//		}
-		
-		byte[] data = Base64Coder.encode(bData);
-
-		long end = System.currentTimeMillis();
-//		System.out.println("Dirty-Rect: " + x1 + "/" + x2 + "  -   " + unionRect.width + "/" + unionRect.height + " Data-Size:" + data.length
-//			+ " took:" + (end - start));
-
-		return new ScreenUpdate(unionRect.x, unionRect.y, data);
 	    }
-	    }
-	    
+
 	    cnt++;
 	    try {
-		Thread.sleep(20); //TODO: Lower
+		Thread.sleep(20);
 	    } catch (InterruptedException e) {
 		e.printStackTrace();
 	    }
@@ -174,17 +158,4 @@ public class ImageStreamer extends HttpServlet {
 
 	return null;
     }
-
-    // static int cnt = 0;
-
-
-    /**
-     * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
-     *      response)
-     */
-    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-	System.out.println("Got GOST");
-	// TODO Auto-generated method stub
-    }
-
 }
