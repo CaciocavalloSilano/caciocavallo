8633b4e341619f35eccfa0a71aca0f34a8999ae0
ceisserer <ceisserer@localhost>
2011-06-30 16:02:47 +0200

    factor session-management out of session-instance-object

--- src/web/classes/net/java/openjdk/awt/peer/web/WebSessionState.java
+++ src/web/classes/net/java/openjdk/awt/peer/web/WebSessionState.java
@@ -1,90 +1,24 @@
 package net.java.openjdk.awt.peer.web;
 
-import java.util.*;
 import java.util.concurrent.locks.*;
 
-import javax.servlet.http.*;
-
-import sun.awt.*;
-
 public class WebSessionState {
-    static final String SESSION_KEY = "WEBSessionState";
-
     ReentrantLock sessionLock = new ReentrantLock();
     WebMouseStateTracker mouseTracker;
     WebKeyboardStateTracker keyboardTracker;
     WebGraphicsConfiguration config;
+    int subSessionID;
 
+    public WebSessionState(int subSessionID) {
+	this.subSessionID = subSessionID;
+    }
+    
     public void lockSession() {
 	sessionLock.lock();
     }
 
     public void unlockSession() {
 	sessionLock.unlock();
-    }
-    
-    public static Integer getSubSessionID(HttpSession session) {
-	List<WebSessionState> subSessionList = (List<WebSessionState>) session.getAttribute(SESSION_KEY);
-	if(subSessionList == null) {
-	    return 0;
-	} else {
-	    synchronized(subSessionList) {
-		return subSessionList.size() - 1;
-	    }
-	}
-    }
-
-    public static Integer register(HttpSession session, Integer subsessionID) {
-	WebSessionState sessionState = null;
-	List<WebSessionState> subSessionList = (List<WebSessionState>) session.getAttribute(SESSION_KEY);
-
-	if(subSessionList == null) {
-	    subSessionList = new ArrayList<WebSessionState>();
-	    session.setAttribute(SESSION_KEY, subSessionList);
-	}
-	
-	synchronized (subSessionList) {
-	    if (subSessionList == null) {
-		subSessionList = new ArrayList<WebSessionState>();
-		session.setAttribute(SESSION_KEY, subSessionList);
-	    }
-
-	    if (subsessionID == null) {
-		subsessionID = subSessionList.size();
-	    } else {
-		sessionState = subSessionList.get(subsessionID);
-	    }
-
-	    if (sessionState == null) {
-		sessionState = new WebSessionState();
-		subSessionList.add(sessionState);
-		AppContext.getAppContext().put(SESSION_KEY, sessionState);
-	    }
-	}
-
-	return subsessionID;
-    }
-
-    public static void unregister() {
-	// synchronized (stateStore) {
-	// WebSessionState state = stateStore.get();
-	// stateStore.set(null);
-	// }
-    }
-
-    public static WebSessionState getCurrentStateAWT() {
-	return (WebSessionState) AppContext.getAppContext().get(SESSION_KEY);
-    }
-    
-    public static WebSessionState getCurrentState(HttpSession session, int subSessionID) {
-	List<WebSessionState> subSessionList = (List<WebSessionState>) session.getAttribute(SESSION_KEY);
-	if(subSessionList != null) {
-	    synchronized(subSessionList) {
-		return subSessionList.get(subSessionID);
-	    }
-	}
-	
-	return null;
     }
 
     public WebGraphicsConfiguration getGraphicsConfiguration() {
@@ -103,5 +37,9 @@ public class WebSessionState {
 
     public WebKeyboardStateTracker getKeyboardTracker() {
 	return keyboardTracker;
+    }
+
+    public int getSubSessionID() {
+        return subSessionID;
     }
 }
