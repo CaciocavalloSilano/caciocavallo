1aaa2f09e667e73600fac7e88c786e5f5c2a7b16
ceisserer <ceisserer@localhost>
2011-07-05 13:21:17 +0200

    start resource-loader servlet by default

--- src/web/classes/net/java/openjdk/cacio/server/CacioServer.java
+++ src/web/classes/net/java/openjdk/cacio/server/CacioServer.java
@@ -1,6 +1,9 @@
 package net.java.openjdk.cacio.server;
 
+import javax.servlet.http.*;
+
 import net.java.openjdk.cacio.servlet.*;
+
 
 import org.eclipse.jetty.server.*;
 import org.eclipse.jetty.server.handler.*;
@@ -22,6 +25,20 @@ public class CacioServer {
         context.addServlet(new ServletHolder(new AppStarter()),"/AppStarter");
         context.addServlet(new ServletHolder(new ImageStreamer()),"/ImageStreamer");
         context.addServlet(new ServletHolder(new EventReceiver()),"/EventReceiver");
+        context.addServlet(new ServletHolder(new ResourceLoaderServlet()),"/ResourceLoader");
+        
+//        context.addEventListener(new HttpSessionListener() {
+//	    
+//	    @Override
+//	    public void sessionDestroyed(HttpSessionEvent ev) {
+//		System.out.println("Session destroyed!");
+//	    }
+//	    
+//	    @Override
+//	    public void sessionCreated(HttpSessionEvent ev) {
+//		System.out.println("Session created!");
+//	    }
+//	});
         
         HandlerList handlers = new HandlerList();
         handlers.setHandlers(new Handler[] { handler, context });
