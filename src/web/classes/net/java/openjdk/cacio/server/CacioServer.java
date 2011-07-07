34ba0c885d9bf02825b366ff0a1b71ab1c83ef84
ceisserer <ceisserer@localhost>
2011-07-07 21:20:22 +0200

    set system properties in server starter

--- src/web/classes/net/java/openjdk/cacio/server/CacioServer.java
+++ src/web/classes/net/java/openjdk/cacio/server/CacioServer.java
@@ -1,10 +1,6 @@
 package net.java.openjdk.cacio.server;
 
-import javax.servlet.http.*;
-
 import net.java.openjdk.cacio.servlet.*;
-
-
 import org.eclipse.jetty.server.*;
 import org.eclipse.jetty.server.handler.*;
 import org.eclipse.jetty.servlet.*;
@@ -12,6 +8,8 @@ import org.eclipse.jetty.servlet.*;
 public class CacioServer {
 
     public CacioServer() throws Exception {
+	applySystemProperties();
+	
         Server server = new Server(8080);
         
         ServletContextHandler context = new ServletContextHandler(ServletContextHandler.SESSIONS);
@@ -27,25 +25,19 @@ public class CacioServer {
         context.addServlet(new ServletHolder(new EventReceiver()),"/EventReceiver");
         context.addServlet(new ServletHolder(new ResourceLoaderServlet()),"/ResourceLoader");
         
-//        context.addEventListener(new HttpSessionListener() {
-//	    
-//	    @Override
-//	    public void sessionDestroyed(HttpSessionEvent ev) {
-//		System.out.println("Session destroyed!");
-//	    }
-//	    
-//	    @Override
-//	    public void sessionCreated(HttpSessionEvent ev) {
-//		System.out.println("Session created!");
-//	    }
-//	});
-        
         HandlerList handlers = new HandlerList();
         handlers.setHandlers(new Handler[] { handler, context });
         server.setHandler(handlers);
         
         server.start();
         server.join();
+    }
+    
+    protected void applySystemProperties() {
+	System.setProperty("awt.useSystemAAFontSettings", "on");
+	System.setProperty("awt.toolkit", "net.java.openjdk.awt.peer.web.WebToolkit");
+	System.setProperty("java.awt.graphicsenv", "net.java.openjdk.awt.peer.web.WebGraphicsEnvironment");
+	System.setProperty("sun.font.fontmanager", "net.java.openjdk.awt.peer.web.WebFontManager");
     }
     
     public static void main(String[] args) throws Exception {
