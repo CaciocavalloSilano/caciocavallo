924e3bfe952defb05d227d9d15d15d06b993e514
ceisserer <ceisserer@localhost>
2011-07-21 18:41:52 +0200

    backend for streaming down RLE encoded images in binary format

---
+++ src/web/classes/net/java/openjdk/cacio/servlet/BinaryRLEStreamEncoder.java
@@ -0,0 +1,30 @@
+package net.java.openjdk.cacio.servlet;
+
+import java.awt.image.*;
+import java.io.*;
+import java.util.*;
+
+import javax.servlet.http.*;
+
+import net.java.openjdk.awt.peer.web.*;
+
+public class BinaryRLEStreamEncoder extends BinaryCmdStreamEncoder {
+
+    @Override
+    public void writeEnocdedData(HttpServletResponse response, List<ScreenUpdate> pendingUpdateList, TreeImagePacker packer, List<Integer> cmdList) throws IOException {
+	DamageRect packedRegionBox = packer.getBoundingBox();
+	
+	long start = System.currentTimeMillis();
+	BufferedImage packedImage = new BufferedImage(packedRegionBox.getWidth(), packedRegionBox.getHeight(), BufferedImage.TYPE_INT_RGB);
+	byte[] cmdStreamData = encodeImageCmdStream(cmdList);
+	copyUpdatesToPackedImage(pendingUpdateList, packedImage, 0);
+	long end = System.currentTimeMillis();
+	System.out.println("Packing took: "+(end-start));
+	
+	response.setContentType("application/binary");
+	OutputStream os = response.getOutputStream();
+	os.write(cmdStreamData);
+	new RLEImageEncoder().encodeImageToStream(packedImage, packedImage.getWidth(), packedImage.getHeight(), os);
+    }
+
+}
