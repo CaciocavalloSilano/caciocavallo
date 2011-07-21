e00cf104d140c50483159d02d239bd5c87147732
ceisserer <ceisserer@localhost>
2011-07-21 15:44:14 +0200

    introduce fast and dynamic byte buffers, which are a lot faster for writing than ByteBufferOutputStream.

---
+++ src/web/classes/net/java/openjdk/cacio/servlet/DynamicByteBuffer.java
@@ -0,0 +1,47 @@
+package net.java.openjdk.cacio.servlet;
+
+import java.io.*;
+import java.util.*;
+
+public class DynamicByteBuffer {
+    final static int BUFFER_SIZE = 8192;
+    
+    ArrayList<byte[]> bufferList = new ArrayList<byte[]>();
+    int size = 0;
+    
+    int curBufferPos = 0;
+    byte[] curBuffer;
+    
+    public DynamicByteBuffer() {
+	addBuffer();
+    }
+    
+    public final void write(byte b) {
+	if(curBufferPos == curBuffer.length) {
+	    addBuffer();
+	}
+	
+	curBuffer[curBufferPos] = b;
+	curBufferPos++;
+    }
+    
+    private void addBuffer() {
+	curBuffer = new byte[BUFFER_SIZE];
+	curBufferPos = 0;
+	bufferList.add(curBuffer);
+    }
+    
+    public int size() {
+	return (bufferList.size() - 1) * BUFFER_SIZE + curBufferPos;
+    }
+    
+    public void writeTo(OutputStream os) throws IOException {
+	for(int i=0; i < bufferList.size(); i++) {
+	    int curBuffLength = Math.min(size - BUFFER_SIZE*i, BUFFER_SIZE);
+	    byte[] curBuff = bufferList.get(i);
+	    os.write(curBuff, 0, curBuffLength);
+	}
+    }
+    
+    
+}
