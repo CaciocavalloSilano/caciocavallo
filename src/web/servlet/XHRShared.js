89f65eac2591f11899e8d1b47f5e2e3299bb78df
ceisserer <ceisserer@localhost>
2011-07-22 20:28:07 +0200

    general refactoring of the backend code

--- src/web/servlet/XHRShared.js
+++ src/web/servlet/XHRShared.js
@@ -1,5 +1,31 @@
 var xmlhttpreq;
 var responseHandlerFunc;
+var intArray;
+
+function readShort(array, pos) {
+	//TODO: Negative Werte behandeln
+	return ((array[pos] << 8) + array[pos + 1]);
+}
+
+function readInt(array, pos) {
+	//TODO: Negative Werte behandeln
+	return ((array[pos] << 24) + (array[pos + 1] << 16) + (array[pos + 2] << 8) + array[pos + 3]);
+}
+
+function readBinCommandStream() {
+	var cmdLength =	readShort(intArray, 0);
+	
+	var shortBuffer = new Array();
+	for(var i=0; i < cmdLength; i++) {
+		shortBuffer[i] = readShort(intArray, (i+1)*2);
+	}
+	
+	var result = new Object();
+	result.shortBuffer = shortBuffer;
+	result.cmdStreamHeight = 0;
+	
+	return result;
+}
 
 function xhrSuccessHandler() {
 	if (xmlhttpreq.readyState==4) {
