56fa5a13d788102c310f2b4b9499a8af700327d3
ceisserer <ceisserer@localhost>
2011-07-22 19:47:26 +0200

    re-use RLE encoder

--- src/web/classes/net/java/openjdk/cacio/servlet/BinaryRLEStreamEncoder.java
+++ src/web/classes/net/java/openjdk/cacio/servlet/BinaryRLEStreamEncoder.java
@@ -3,12 +3,15 @@ package net.java.openjdk.cacio.servlet;
 import java.awt.image.*;
 import java.io.*;
 import java.util.*;
-
-import javax.servlet.http.*;
-
 import net.java.openjdk.awt.peer.web.*;
 
 public class BinaryRLEStreamEncoder extends BinaryCmdStreamEncoder {
+
+    RLEImageEncoder rleEncoder;
+
+    public BinaryRLEStreamEncoder() {
+	rleEncoder = new RLEImageEncoder();
+    }
 
     @Override
     public void writeEnocdedData(OutputStream os, List<ScreenUpdate> pendingUpdateList, TreeImagePacker packer, List<Integer> cmdList)
@@ -18,18 +21,42 @@ public class BinaryRLEStreamEncoder extends BinaryCmdStreamEncoder {
 	byte[] cmdStreamData = encodeImageCmdStream(cmdList);
 	os.write(cmdStreamData);
 
-//	long start = System.currentTimeMillis();
-	if (pendingUpdateList.size() == 1 && pendingUpdateList.get(0) instanceof BlitScreenUpdate) {
-	    BlitScreenUpdate update = (BlitScreenUpdate) pendingUpdateList.get(0);
-	    new RLEImageEncoder().encodeImageToStream(update.getImage(), update.getSrcX(), update.getSrcY(), update.getSrcX() + update.getUpdateArea().getWidth(), update.getSrcY() + update.getUpdateArea().getHeight(), os);
-//	    System.out.println("Took fast path");
+	// Fast-Path: If there is only a single BlitScreenUpdate, encode
+	// directly from the SurfaceData and avoid an additional blit
+	BlitScreenUpdate singleUpdate = getLonelyBlitScreenUpdate(pendingUpdateList);
+	if (singleUpdate != null) {
+	    DamageRect updateArea = singleUpdate.getUpdateArea();
+	    rleEncoder.encodeImageToStream(singleUpdate.getImage(), singleUpdate.getSrcX(), singleUpdate.getSrcY(), singleUpdate.getSrcX()
+		    + updateArea.getWidth(), singleUpdate.getSrcY() + updateArea.getHeight(), os);
 	} else {
 	    BufferedImage packedImage = new BufferedImage(packedRegionBox.getWidth(), packedRegionBox.getHeight(), BufferedImage.TYPE_INT_RGB);
 	    copyUpdatesToPackedImage(pendingUpdateList, packedImage, 0);
-	    new RLEImageEncoder().encodeImageToStream(packedImage, 0, 0, packedImage.getWidth(), packedImage.getHeight(), os);
+	    rleEncoder.encodeImageToStream(packedImage, 0, 0, packedImage.getWidth(), packedImage.getHeight(), os);
 	}
 
-//	long end = System.currentTimeMillis();
-//	System.out.println("Packing took: " + (end - start));
+    }
+
+    /**
+     * Returns the one and only BlitScreenUpdate of a list, or null if the List
+     * has more than one BlitScreenCommands
+     * 
+     * @param updateList
+     * @return
+     */
+    protected BlitScreenUpdate getLonelyBlitScreenUpdate(List<ScreenUpdate> updateList) {
+	BlitScreenUpdate lonelyUpdate = null;
+
+	for (ScreenUpdate update : updateList) {
+	    if (update instanceof BlitScreenUpdate) {
+		// If there is more than one BlitScreenUpdate, bail out
+		if (lonelyUpdate != null) {
+		    lonelyUpdate = null;
+		    break;
+		}
+		lonelyUpdate = (BlitScreenUpdate) update;
+	    }
+	}
+
+	return lonelyUpdate;
     }
 }
