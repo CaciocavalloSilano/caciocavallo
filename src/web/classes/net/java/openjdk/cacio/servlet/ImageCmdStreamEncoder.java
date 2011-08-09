d2e93fb8a2c898b31429d418fd5a8bda0225096e
ceisserer <ceisserer@localhost>
2011-08-09 22:58:37 +0200

    fix small typo in method name

--- src/web/classes/net/java/openjdk/cacio/servlet/ImageCmdStreamEncoder.java
+++ src/web/classes/net/java/openjdk/cacio/servlet/ImageCmdStreamEncoder.java
@@ -38,7 +38,7 @@ public class ImageCmdStreamEncoder extends CmdStreamEncoder {
 	}
     }
     
-    public void writeEnocdedData(OutputStream os, List<ScreenUpdate> pendingUpdateList, TreeImagePacker packer, List<Integer> cmdList) throws IOException {
+    public void writeEncodedData(OutputStream os, List<ScreenUpdate> pendingUpdateList, TreeImagePacker packer, List<Integer> cmdList) throws IOException {
 	DamageRect packedRegionBox = packer.getBoundingBox();
 	int regionWidth = packedRegionBox.getWidth() != 0 ? packedRegionBox.getWidth() : 16;
 	int regionHeight = packedRegionBox.getHeight();
