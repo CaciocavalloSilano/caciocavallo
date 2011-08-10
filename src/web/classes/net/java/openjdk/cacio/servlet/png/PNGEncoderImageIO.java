1357b7dd78e88495e484f150c9d4a9cc57b4a112
ceisserer <ceisserer@localhost>
2011-08-10 11:42:02 +0200

    factor out png encoder and default to ImageIO, to reduce external dependencies

---
+++ src/web/classes/net/java/openjdk/cacio/servlet/png/PNGEncoderImageIO.java
@@ -0,0 +1,23 @@
+package net.java.openjdk.cacio.servlet.png;
+
+import java.awt.image.*;
+import java.io.*;
+
+import javax.imageio.*;
+
+public class PNGEncoderImageIO extends PNGEncoder {
+
+    @Override
+    public byte[] encode(BufferedImage img, int compression) {
+	ByteArrayOutputStream bos = new ByteArrayOutputStream((img.getWidth() * img.getHeight()) / 2);
+
+	try {
+	    ImageIO.write(img, "PNG", bos);
+	} catch (IOException e) {
+	    e.printStackTrace();
+	}
+	
+	return bos.toByteArray();
+    }
+
+}
