8c1213df6a31efff6527e26e2fe6a189d4f03c0c
ceisserer <ceisserer@localhost>
2011-08-12 11:09:18 +0200

    adapt session-timeout to 30s, to get rid of non-active sessions as soon as possible

--- src/web/classes/net/java/openjdk/cacio/server/CacioServer.java
+++ src/web/classes/net/java/openjdk/cacio/server/CacioServer.java
@@ -26,7 +26,7 @@ public class CacioServer {
         ServletContextHandler context = new ServletContextHandler(ServletContextHandler.SESSIONS);
         context.setContextPath("/");
         context.setResourceBase("bin/");
-        context.getSessionHandler().getSessionManager().setMaxInactiveInterval(600);
+        context.getSessionHandler().getSessionManager().setMaxInactiveInterval(30);
         context.getSessionHandler().addEventListener(new CacioSessionListener());
         
         ResourceHandler handler = new ResourceHandler();
