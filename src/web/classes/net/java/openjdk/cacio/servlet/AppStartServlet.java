318d82db395700565b53a863d8d82b8a90c57fdc
ceisserer <ceisserer@localhost>
2011-08-16 12:11:27 +0200

    rename a few occurances of "backend" to "transport"

--- src/web/classes/net/java/openjdk/cacio/servlet/AppStartServlet.java
+++ src/web/classes/net/java/openjdk/cacio/servlet/AppStartServlet.java
@@ -49,11 +49,11 @@ public class AppStartServlet extends SubSessionServletBase {
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 	int width = Integer.parseInt(request.getParameter("w"));
 	int height = Integer.parseInt(request.getParameter("h"));
-	String backend = request.getParameter("b");
+	String transport = request.getParameter("t");
 
 	WebSessionState state = getSessionState(request);
 	state.setInitialScreenDimension(new Dimension(width, height));
-	state.setBackend(Transport.getBackendForName(backend, state.getCompressLevel()));
+	state.setBackend(Transport.getTransportForName(transport, state.getCompressLevel()));
 	new AppContextCreator().startAppInNewAppContext(state);
     }
 }
--- src/web/classes/net/java/openjdk/cacio/servlet/transport/Transport.java
+++ src/web/classes/net/java/openjdk/cacio/servlet/transport/Transport.java
@@ -33,22 +33,27 @@ import java.util.List;
 import net.java.openjdk.awt.peer.web.*;
 
 /**
+ * Base-Class of all Transport implementations. A transpor defines the way the
+ * image-data as well as the command-stream are encoded and sent to the client.
+ * 
+ * Each transport consists of its javascript counterpart, so the browser-client
+ * can deal with the data it receives.
  * 
  * @author Clemens Eisserer <linuxhippy@gmail.com>
  */
 public abstract class Transport {
-    
+
     public static final String FORMAT_PNG_BASE64 = "base64";
     public static final String FORMAT_PNG_IMG = "img";
     public static final String FORMAT_PNG_XHR = "png";
     public static final String FORMAT_RLE = "rle";
-    
+
     String contentType;
-    
+
     public Transport(String contentType) {
 	this.contentType = contentType;
     }
-    
+
     protected void copyUpdatesToPackedImage(List<ScreenUpdate> updateList, BufferedImage packedImage, int packedAreaHeight) {
 	Graphics g = packedImage.getGraphics();
 
@@ -59,26 +64,36 @@ public abstract class Transport {
 		int width = bsUpdate.getUpdateArea().getWidth();
 		int height = bsUpdate.getUpdateArea().getHeight();
 
-		g.drawImage(bsUpdate.getImage(), bsUpdate.getPackedX(), bsUpdate.getPackedY() + packedAreaHeight, bsUpdate.getPackedX() + width, bsUpdate.getPackedY()+ height + packedAreaHeight, bsUpdate.getSrcX(), bsUpdate.getSrcY(),
-			bsUpdate.getSrcX() + width, bsUpdate.getSrcY() + height, null);
+		g.drawImage(bsUpdate.getImage(), bsUpdate.getPackedX(), bsUpdate.getPackedY() + packedAreaHeight, bsUpdate.getPackedX() + width,
+			bsUpdate.getPackedY() + height + packedAreaHeight, bsUpdate.getSrcX(), bsUpdate.getSrcY(), bsUpdate.getSrcX() + width,
+			bsUpdate.getSrcY() + height, null);
 	    }
 	}
     }
-    
-    public abstract void writeEncodedData(OutputStream os, List<ScreenUpdate> pendingUpdateList, TreeImagePacker packer, List<Integer> cmdData) throws IOException;
-    
+
+    public abstract void writeEncodedData(OutputStream os, List<ScreenUpdate> pendingUpdateList, TreeImagePacker packer, List<Integer> cmdData)
+	    throws IOException;
+
     public abstract void writeEmptyData(OutputStream os) throws IOException;
 
+    /**
+     * @return the HTTP content type the transport outputs.
+     */
     public String getContentType() {
-        return contentType;
+	return contentType;
     }
-    
-    public static Transport getBackendForName(String backendName, int compressionLevel) {
-	if(backendName.equalsIgnoreCase(FORMAT_RLE)) {
+
+    /**
+     * @param backendName
+     * @param compressionLevel
+     * @return
+     */
+    public static Transport getTransportForName(String backendName, int compressionLevel) {
+	if (backendName.equalsIgnoreCase(FORMAT_RLE)) {
 	    return new BinaryRLETransport();
-	} else if(backendName.equalsIgnoreCase(FORMAT_PNG_XHR)) {
+	} else if (backendName.equalsIgnoreCase(FORMAT_PNG_XHR)) {
 	    return new BinaryPngTransport(compressionLevel);
-	} else if(backendName.equalsIgnoreCase(FORMAT_PNG_IMG)) {
+	} else if (backendName.equalsIgnoreCase(FORMAT_PNG_IMG)) {
 	    return new ImageTransport(compressionLevel);
 	} else {
 	    return new Base64PngTransport(compressionLevel);
