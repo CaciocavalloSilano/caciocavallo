23da7af40146271763e4d89d3e4adda4bb104ea9
rkennke <rkennke@localhost>
2011-11-07 20:57:07 +0100

    Fix locale specific caching of keystroke mappings.

--- cacio-tta/src/main/java/net/java/openjdk/cacio/ctc/KeyStrokeMappingFactory.java
+++ cacio-tta/src/main/java/net/java/openjdk/cacio/ctc/KeyStrokeMappingFactory.java
@@ -1,25 +1,29 @@
 package net.java.openjdk.cacio.ctc;
 
+import java.util.HashMap;
 import java.util.Locale;
+import java.util.Map;
 
 class KeyStrokeMappingFactory {
 
     private static KeyStrokeMappingFactory instance = new KeyStrokeMappingFactory();
 
-    private KeyStrokeMapping mapping;
+    private Map<String,KeyStrokeMapping> maps = new HashMap<String,KeyStrokeMapping>();
 
     static KeyStrokeMappingFactory getInstance() {
         return instance;
     }
 
     KeyStrokeMapping getKeyStrokeMapping() {
+        String lang = Locale.getDefault().getCountry().toLowerCase();
+        KeyStrokeMapping mapping = maps.get(lang);
         if (mapping == null) {
-            Locale locale = Locale.getDefault();
-            if (locale.getCountry().equals("DE")) {
+            if (lang.equals("de")) {
                 mapping = new KeyStrokeMappingDE();
             } else {
                 mapping = new KeyStrokeMappingEN();
             }
+            maps.put(lang, mapping);
         }
         return mapping;
     }
