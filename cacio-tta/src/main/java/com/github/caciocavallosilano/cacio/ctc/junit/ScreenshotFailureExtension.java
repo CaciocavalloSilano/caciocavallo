a58e69dc9e50c270625efffbcaf1c7b09e8b0584
Glen Schrader <gschrader@gmail.com>
2023-08-28 17:12:31 -0600

    perform screenshots with failures from LifecycleMethodExecutionExceptionHandler, i.e. @Before set the JUL anonymous logger to FINE so that the log statements can be logged

--- cacio-tta/src/main/java/com/github/caciocavallosilano/cacio/ctc/junit/ScreenshotFailureExtension.java
+++ cacio-tta/src/main/java/com/github/caciocavallosilano/cacio/ctc/junit/ScreenshotFailureExtension.java
@@ -26,26 +26,68 @@ package com.github.caciocavallosilano.cacio.ctc.junit;
 
 import org.assertj.swing.junit.runner.FailureScreenshotTaker;
 import org.assertj.swing.junit.runner.ImageFolderCreator;
-import org.junit.jupiter.api.extension.ExtensionContext;
-import org.junit.jupiter.api.extension.TestExecutionExceptionHandler;
+import org.junit.jupiter.api.extension.*;
+
+import java.lang.reflect.Field;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 import static org.assertj.swing.annotation.GUITestFinder.isGUITest;
 import static org.assertj.swing.junit.runner.Formatter.testNameFrom;
 
-public class ScreenshotFailureExtension implements TestExecutionExceptionHandler {
+public class ScreenshotFailureExtension implements TestExecutionExceptionHandler, LifecycleMethodExecutionExceptionHandler {
 
     private final FailureScreenshotTaker screenshotTaker;
 
     public ScreenshotFailureExtension() {
         System.setProperty("org.assertj.swing.preserve_screenshots", "true");
         this.screenshotTaker = new FailureScreenshotTaker(new ImageFolderCreator().createImageFolder());
+
+        try {
+            // yuck, the anonymous logger level can't be set
+            // the jul logger seems to work for tests in this repos, but it doesn't when using as a library, maybe because of logback/slf4j
+            Field loggerField = FailureScreenshotTaker.class.getDeclaredField("logger");
+            loggerField.setAccessible(true);
+            Logger logger = (Logger) loggerField.get(null);
+            logger.setLevel(Level.FINE);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
     }
 
     @Override
     public void handleTestExecutionException(ExtensionContext context, Throwable throwable) throws Throwable {
+        takeScreenshot(context);
+        throw throwable;
+    }
+
+    private void takeScreenshot(ExtensionContext context) {
         if (isGUITest(context.getRequiredTestClass(), context.getRequiredTestMethod())) {
             screenshotTaker.saveScreenshot(testNameFrom(context.getRequiredTestClass(), context.getRequiredTestMethod()));
         }
+    }
+
+    @Override
+    public void handleBeforeAllMethodExecutionException(ExtensionContext context, Throwable throwable) throws Throwable {
+        takeScreenshot(context);
+        throw throwable;
+    }
+
+    @Override
+    public void handleBeforeEachMethodExecutionException(ExtensionContext context, Throwable throwable) throws Throwable {
+        takeScreenshot(context);
+        throw throwable;
+    }
+
+    @Override
+    public void handleAfterEachMethodExecutionException(ExtensionContext context, Throwable throwable) throws Throwable {
+        takeScreenshot(context);
+        throw throwable;
+    }
+
+    @Override
+    public void handleAfterAllMethodExecutionException(ExtensionContext context, Throwable throwable) throws Throwable {
+        takeScreenshot(context);
         throw throwable;
     }
 }
