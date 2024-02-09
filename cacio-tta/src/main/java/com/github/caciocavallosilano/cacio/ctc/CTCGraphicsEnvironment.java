f50727aeacbc85f972601e1ec676b386a55bd839
Jan Blom <jan.j.blom@gmail.com>
2024-02-09 11:37:54 +0100

    Keep constructor public, no need to change the API

--- cacio-tta/src/main/java/com/github/caciocavallosilano/cacio/ctc/CTCGraphicsEnvironment.java
+++ cacio-tta/src/main/java/com/github/caciocavallosilano/cacio/ctc/CTCGraphicsEnvironment.java
@@ -32,7 +32,7 @@ import sun.java2d.SurfaceManagerFactory;
 public class CTCGraphicsEnvironment extends SunGraphicsEnvironment {
 
     private static final CTCGraphicsEnvironment INSTANCE = new CTCGraphicsEnvironment();
-    private CTCGraphicsEnvironment() {
+    public CTCGraphicsEnvironment() {
         SurfaceManagerFactory.setInstance(new CTCSurfaceManagerFactory());
     }
 
